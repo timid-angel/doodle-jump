98b349f24dca60ffebbbf66f42ab6a4bbae84cd1
timid_angel <natms24@gmail.com>
2023-10-10 20:02:20 +0300

    Added basic movement

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let doodlerBottomSpace = 250
     let isGameOver = false
     let platformCount = 5
     let platforms = []
+    let upTimerId
+    let downTimerid
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -37,10 +39,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+        }, 30)
+    }
+
     function start() {
         if (!isGameOver) {
             createDoodler()
             createPlatforms()
+            setInterval(movePlatforms,30)
+            jump()
         }
     }
     // attach to a button
